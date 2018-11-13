b7cd6998912016a29b3939e52b5f3ad00fd384ea
yangli <1852627453@qq.com>
2018-11-13 14:54:57 +0800

    编辑员工功能完成

--- src/main/java/com/yangli/springbootWeb/controller/EmployeeController.java
+++ src/main/java/com/yangli/springbootWeb/controller/EmployeeController.java
@@ -1,11 +1,14 @@
 package com.yangli.springbootWeb.controller;
 
+import com.yangli.springbootWeb.dao.DepartmentDao;
 import com.yangli.springbootWeb.dao.EmployeeDao;
+import com.yangli.springbootWeb.entities.Department;
 import com.yangli.springbootWeb.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.Collection;
 
@@ -20,6 +23,8 @@ import java.util.Collection;
 public class EmployeeController {
     @Autowired
     EmployeeDao employeeDao;
+    @Autowired
+    DepartmentDao departmentDao;
 
     @GetMapping("/getEmployees")
     public String getList(Model model){
@@ -42,10 +47,36 @@ public class EmployeeController {
         return "redirect:/getEmployees";
     }
 
+    /**
+     * 删除员工
+     * @param id
+     * @return
+     */
     @GetMapping("/delEmployee")
     public String delEmployee(Integer id){
         employeeDao.delete(id);
         return "redirect:/getEmployees";
     }
 
+    @GetMapping("/updateEmployee")
+    public String updateEmployee(Integer id,Model model){
+        System.err.println(id);
+        Employee employee = employeeDao.get(id);
+        model.addAttribute("employee",employee);
+        return "/emps/updateEmployee";
+    }
+
+    @GetMapping("/toUpdateEmployee")
+    public String toUpdateEmployee(Employee employee) {
+        System.err.println(employee);
+        Employee employee1 = employeeDao.get(employee.getId());
+        System.err.println(employee1);
+        employee1.setEmail(employee.getEmail());
+        employee1.setLastName(employee.getLastName());
+        Department department = departmentDao.getDepartment(employee.getDepartment().getId());
+        employee1.setDepartment(department);
+        employee1.setGender(employee.getGender());
+        employee1.setBirth(employee.getBirth());
+        return "redirect:/getEmployees";
+    }
 }
